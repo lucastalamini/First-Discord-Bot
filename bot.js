998a4e0eb564d5f6d4e7083889eae4c48f4957bf
Lucas Talamini <lucastalamini@hotmail.com>
2020-12-02 16:06:56 -0300

    Music bot test

--- bot.js
+++ bot.js
@@ -1,18 +1,138 @@
 require("dotenv").config();
 
 const Discord = require("discord.js");
+const prefix = require("./config.json");
+const ytdl = require("ytdl-core");
 const client = new Discord.Client({
   partials: ["MESSAGE"],
+});
+
+client.login(process.env.BOT_TOKEN);
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
 });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
-  if (msg.content === `ping`) {
-    msg.reply("Pong!");
+client.on("message", async (message) => {
+  // Ignore itself
+  if (message.author.bot) return;
+  // Check if using prefix
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content === "test") {
+    return message.channel.send("Success!");
+  }
+
+  // Music bot
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}slip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+  } else {
+    message.channel.send("You need to enter a valid command!");
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url,
+  };
+
+  if (!serverQueue) {
+    const queueContract = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connections: null,
+      songs: [],
+      volume: 5,
+      playing: true,
+    };
+
+    queue.set(message.guild.id, queueContract);
+
+    queueContract.songs.push(song);
+
+    try {
+      let = connection = await voiceChannel.join();
+      queueContract.connection = connection;
+      play(message.guild, queueContract.songs[0]);
+    } catch (error) {
+      console.log(error);
+      queue.delete(message.guild.id);
+      return message.channel.send(error);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to skip a song!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There are no songs in the queue :|");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the jamming!"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guid.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", (error) => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Current song: ${song.title}`);
+}
